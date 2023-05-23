e0d652383ab643ad2d410d51053ff6fabf7e7e13
ASEK88 <artursek8@gmail.com>
2023-05-23 22:53:37 +0200

    EWA

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Kobietra poniżej 30 lat
+// Ewa, lat 33
+//Niepełnoletni mężczyzna 
+
+string Name = "Ewa";
+int Age = 33;
+string Gender = "Kobieta";
+
+if ( Name == "Ewa" && Age == 33)
+{ 
+    Console.WriteLine( "Ewa, lat 33" );
+}
+if ( Gender == "Kobieta" && Age > 30 )
+{
+    Console.WriteLine( "Kobieta poniżej 30 lat" );
+}
+if (!(Gender == "Kobieta" && Age < 18))
+{
+    Console.WriteLine("Niepełnoletni Meżczyzna");
+}
+if (!(Gender == "Kobieta" && Name == "Ewa" && Age >33 ))
+{
+    Console.WriteLine ( "100 letni Marek" );
+}
