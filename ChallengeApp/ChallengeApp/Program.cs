68ca70711683db055d86a4c24bf3badfda8f6f9c
ASEK88 <artursek8@gmail.com>
2023-05-24 23:11:45 +0200

    Ewa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,20 @@
-﻿// Przygotuj program który policzy ile jakich cyfr występuje w liczbie:
-//
-//Przykład:
-//Wynik dla liczby: 4566
-// 0 => 0
-// 1 => 0
-// 2 => 0
-// 3 => 0
-// 4 => 1
-// 5 => 1
-// 6 => 2
-// 7 => 0 
-// 8 => 0
-// 9 => 0
+﻿// "Kobieta poniżej 30 lat"
+// "Ewa lat 33"
+// "Niepełnoletni mężczyzna"
 
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+String Name = "Ewa";
+int Age =33;
+string Gender = "Kobieta";
 
-for (int i = 0; i >10; i++)
+if (Name == "Ewa" && Age == 33)
 {
-    Console.WriteLine(letters[i]);
+    Console.WriteLine("Ewa lat 33");
+}
+else if (Gender == "Mężczyzna" && Age < 18)
+{
+    Console.WriteLine("Niepłnoletni Męższczyzna");
+}
+if (Age <30 && Gender == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30");
 }
